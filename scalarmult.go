dfda9dd96589cf74cdd9a9584b8b12c9e99ba625
Tony Rizko <tony@rizkocircle.com>
2015-07-29 22:13:35 -0700

    adds ScalarMult function

---
+++ scalarmult.go
@@ -0,0 +1,23 @@
+// Copyright 20015 Tony Rizko. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+// Package linear contains tools to implement machine
+// learning in a Go environment.
+package linear
+
+// ScalarMult multiplies a scalar number and matrix and returns the product
+func ScalarMult(scalar float64, matrix [][]float64) [][]float64 {
+	var result [][]float64
+
+	for i, row := range matrix {
+		if i == 0 {
+			result = matrix
+		}
+		for j, val := range row {
+			result[i][j] = scalar * val
+		}
+	}
+
+	return result
+}
